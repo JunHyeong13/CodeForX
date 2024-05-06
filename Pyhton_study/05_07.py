605f95d72bd67616345dc1f164d2a1a02d57430a
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2024-05-06 22:25:57 +0900

    CodingTest_Programmers
    Programmers 1일차

---
+++ Pyhton_study/05_07.py
@@ -0,0 +1,11 @@
+# Programmers 기초문항 1
+
+def solution(strArr):
+    answer = []
+
+    for idx, val in enumerate(strArr):
+        if idx % 2 == 0:
+            answer.append(val.lower())
+        else:
+            answer.append(val.upper())
+    return answer
