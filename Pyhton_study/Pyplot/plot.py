85ab96c7759fedcd4be7f985b6d00b42a78e1da5
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-01-15 21:32:13 +0900

    new code

--- Pyhton_study/Pyplot/plot.py
+++ Pyhton_study/Pyplot/plot.py
@@ -1,34 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 
-# # 데이터 설정
-# y_values = range(1, 7)
-# x_start = [0.25,0,0.85,0,0,0] # 0, 0.25, 0, 0, 0, 0, 0.65, 0, 0, 0.65
-# x_end = [0.25, 1, 1, 1, 1, 0.55, 1, 1, 0.55, 1] # 0.25, 1, 1, 1, 1, 0.55, 1, 1, 0.55, 1
-# x_lengths = [end - start for start, end in zip(x_start, x_end)]
-
-# # 막대 색상 설정 (파란색)
-# color = 'blue'
-
-# # 가로 막대 그래프 생성
-# plt.figure(figsize=(8, 6))
-# plt.barh(y_values, x_lengths, color=color, edgecolor="black", left=x_start)
-
-# # 축 설정
-# plt.xlabel("Time (seconds)")
-# plt.ylabel("Items (1-6)")
-# plt.xlim(0, 3.8)  # 최대 시간을 조금 더 여유 있게 설정
-# plt.ylim(0.5, 6.5)
-# plt.xticks(np.arange(0, 3.8, 0.5))
-# plt.yticks(y_values)
-
-# # 그래프 제목
-# plt.title("Surprised(KSL)")
-
-# # 그래프 표시
-# plt.tight_layout()
-# plt.show()
-
 # Create the figure and axes
 fig, ax = plt.subplots(figsize=(8, 4))
 
@@ -44,6 +16,7 @@ y_segments = {
 for i, (label, segments) in enumerate(y_segments.items()):
     for start, end in segments:
         ax.barh(label, end - start, left=start, color='blue')
+        # Intensity 값을 얼마나 주었는지 보여줄 수 있도록 코드. 
 
 # Set x and y axis limits
 ax.set_xlim(0, 3.5)
@@ -58,6 +31,37 @@ ax.set_xlabel("Time (seconds)")
 ax.set_ylabel("Items (1-6)")
 
 # Display the plot
-plt.title("Sadness(KSL)")
+plt.title("Sadness(KSL)") #Sadness(KSL), Anger(KSL), Disgust(KSL), Amusement(KSL), Surprised(KSL), Fear(KSL)
 plt.tight_layout()
 plt.show()
+
+
+'''
+# 데이터 설정
+y_values = range(1, 7)
+x_start = [0.25,0,0.85,0,0,0] # 0, 0.25, 0, 0, 0, 0, 0.65, 0, 0, 0.65
+x_end = [0.25, 1, 1, 1, 1, 0.55, 1, 1, 0.55, 1] # 0.25, 1, 1, 1, 1, 0.55, 1, 1, 0.55, 1
+x_lengths = [end - start for start, end in zip(x_start, x_end)]
+
+# 막대 색상 설정 (파란색)
+color = 'blue'
+
+# 가로 막대 그래프 생성
+plt.figure(figsize=(8, 6))
+plt.barh(y_values, x_lengths, color=color, edgecolor="black", left=x_start)
+
+# 축 설정
+plt.xlabel("Time (seconds)")
+plt.ylabel("Items (1-6)")
+plt.xlim(0, 3.8)  # 최대 시간을 조금 더 여유 있게 설정
+plt.ylim(0.5, 6.5)
+plt.xticks(np.arange(0, 3.8, 0.5))
+plt.yticks(y_values)
+
+# 그래프 제목
+plt.title("Surprised(KSL)")
+
+# 그래프 표시
+plt.tight_layout()
+plt.show()
+'''
