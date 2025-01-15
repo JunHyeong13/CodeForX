8a62334d902038cad11f6dd67d5418b0b9b3c1fc
JunHyeong13 <whwnsgud13@hanyang.ac.kr>
2025-01-15 21:30:37 +0900

    푸는 중..

--- Pyhton_study/Coding_입문/10811.py
+++ Pyhton_study/Coding_입문/10811.py
@@ -5,7 +5,7 @@ store = [i for i in range(1, n+1)]
 temp = 0 # 역순을 하게 되면, 잠시 저장해둬야 하는 공간이 필요하므로. 
 for i in range(m):
     i,j = map(int, input().split())
-    temp = store[i-1:j]
+    temp = store[i-1:j] # 슬라이싱 문제 였음..
     temp.reverse() # 역순을 해주는 구간. 
     print(temp)
     store[i-1:j] = temp
